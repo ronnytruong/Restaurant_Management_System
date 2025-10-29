9a3044ff5bc47a58b4305588ad6a5fb5ca72253f
Truong Binh Trong <trongtb.ce190757@gmail.com>
2025-10-29 15:04:52 +0700

    Update UI reservation

---
+++ src/main/java/controller/ReservationServlet.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import static constant.CommonFunction.getTotalPages;
+import static constant.CommonFunction.validateString;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author TruongBinhTrong
+ */
+@WebServlet(name = "ReservationServlet", urlPatterns = {"/reservation"})
+public class ReservationServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ReservationServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ReservationServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String namepage = "";
+        String view = request.getParameter("view");
+
+        if (!validateString(view, -1) || view.equalsIgnoreCase("booktable")) {
+            namepage = "booktable";
+        } else if (view.equalsIgnoreCase("add")) {
+            namepage = "add";
+        } else if (view.equalsIgnoreCase("edit")) {
+            namepage = "edit";
+
+            int id;
+
+            try {
+                id = Integer.parseInt(request.getParameter("id"));
+            } catch (NumberFormatException e) {
+                id = -1;
+            }
+
+//            request.setAttribute("currentSupplier", supplierDAO.getElementByID(id));
+        } else if (view.equalsIgnoreCase("delete")) {
+            namepage = "delete";
+        }
+
+        int page;
+//        int totalPages = getTotalPages(supplierDAO.countItem());
+
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (NumberFormatException e) {
+            page = 1;
+        }
+
+//        request.setAttribute("totalPages", totalPages);
+//        request.setAttribute("suppliersList", supplierDAO.getAll());
+
+        request.getRequestDispatcher("/WEB-INF/reservation/" + namepage + ".jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
