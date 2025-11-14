eba5f102ab1df99da3503541b7a9e618b7510c4d
Dai Minh Nhu <NhuDM.CE190213@gmail.com>
2025-11-14 18:28:14 +0700

    them phan quyen

--- src/main/java/utils/LoginFilter.java
+++ src/main/java/utils/LoginFilter.java
@@ -2,35 +2,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
  */
-package filter;
+package utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 
 /**
  *
  * @author Dai Minh Nhu - CE190213
  */
-@WebFilter(filterName = "LoginFilter", urlPatterns = {"/recipe"})
+@WebFilter(filterName = "LoginFilter", urlPatterns = {
+    "/dashboard", "/orderItem", "/order", "/reservation", "/table",
+    "/category", "/menuItem", "/type", "/ingredient", "/recipe",
+    "/import", "/supplier", "/employee", "/customer", "/role", "/voucher", "/homepage"})
 public class LoginFilter implements Filter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
-    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("doFilter called");
+        System.out.println("LoginFilter called");
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
 
@@ -40,13 +38,8 @@ public class LoginFilter implements Filter {
             resp.sendRedirect(req.getContextPath() + "/login");
             return;
         }
-        
-        chain.doFilter(request, response);
-    }
 
-    @Override
-    public void destroy() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        chain.doFilter(request, response);
     }
 
 }
